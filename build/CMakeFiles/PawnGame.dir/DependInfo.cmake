563dbc32bc5769918bfd699e81c3ec5544fd038b
Marco Bobr <marrcokyr@gmail.com>
2024-12-09 20:53:41 +0100

    gitignore pishov nahui

---
+++ build/CMakeFiles/PawnGame.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/marrc/bul/cpp-project/alt/ludo/src/Board.cpp" "CMakeFiles/PawnGame.dir/src/Board.cpp.obj" "gcc" "CMakeFiles/PawnGame.dir/src/Board.cpp.obj.d"
+  "C:/Users/marrc/bul/cpp-project/alt/ludo/src/EventDispatcher.cpp" "CMakeFiles/PawnGame.dir/src/EventDispatcher.cpp.obj" "gcc" "CMakeFiles/PawnGame.dir/src/EventDispatcher.cpp.obj.d"
+  "C:/Users/marrc/bul/cpp-project/alt/ludo/src/EventLoop.cpp" "CMakeFiles/PawnGame.dir/src/EventLoop.cpp.obj" "gcc" "CMakeFiles/PawnGame.dir/src/EventLoop.cpp.obj.d"
+  "C:/Users/marrc/bul/cpp-project/alt/ludo/src/MovePawnHandler.cpp" "CMakeFiles/PawnGame.dir/src/MovePawnHandler.cpp.obj" "gcc" "CMakeFiles/PawnGame.dir/src/MovePawnHandler.cpp.obj.d"
+  "C:/Users/marrc/bul/cpp-project/alt/ludo/src/PawnManager.cpp" "CMakeFiles/PawnGame.dir/src/PawnManager.cpp.obj" "gcc" "CMakeFiles/PawnGame.dir/src/PawnManager.cpp.obj.d"
+  "C:/Users/marrc/bul/cpp-project/alt/ludo/src/PlayerTurnHandler.cpp" "CMakeFiles/PawnGame.dir/src/PlayerTurnHandler.cpp.obj" "gcc" "CMakeFiles/PawnGame.dir/src/PlayerTurnHandler.cpp.obj.d"
+  "C:/Users/marrc/bul/cpp-project/alt/ludo/src/RollDiceHandler.cpp" "CMakeFiles/PawnGame.dir/src/RollDiceHandler.cpp.obj" "gcc" "CMakeFiles/PawnGame.dir/src/RollDiceHandler.cpp.obj.d"
+  "C:/Users/marrc/bul/cpp-project/alt/ludo/src/StopGameHandler.cpp" "CMakeFiles/PawnGame.dir/src/StopGameHandler.cpp.obj" "gcc" "CMakeFiles/PawnGame.dir/src/StopGameHandler.cpp.obj.d"
+  "C:/Users/marrc/bul/cpp-project/alt/ludo/src/TurnManager.cpp" "CMakeFiles/PawnGame.dir/src/TurnManager.cpp.obj" "gcc" "CMakeFiles/PawnGame.dir/src/TurnManager.cpp.obj.d"
+  "C:/Users/marrc/bul/cpp-project/alt/ludo/src/WaitForChoosePawnEvent.cpp" "CMakeFiles/PawnGame.dir/src/WaitForChoosePawnEvent.cpp.obj" "gcc" "CMakeFiles/PawnGame.dir/src/WaitForChoosePawnEvent.cpp.obj.d"
+  "C:/Users/marrc/bul/cpp-project/alt/ludo/src/main.cpp" "CMakeFiles/PawnGame.dir/src/main.cpp.obj" "gcc" "CMakeFiles/PawnGame.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PawnGame.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for PawnGame.
+# This may be replaced when dependencies are built.
